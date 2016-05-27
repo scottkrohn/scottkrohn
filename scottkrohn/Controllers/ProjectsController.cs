8bc4aa5320da0c398d16fe0cad09cf584c7f3e67
Scott Krohn <skrohn86@gmail.com>
2016-05-27 16:07:05 -0700

    Added another project to projects page

--- scottkrohn/Controllers/ProjectsController.cs
+++ scottkrohn/Controllers/ProjectsController.cs
@@ -19,76 +19,9 @@ namespace scottkrohn.Controllers
 			return View();
 		}
 
-        // GET: Projects/Details/5
-        public ActionResult Details(int id)
-        {
-            return View();
-        }
-
-        // GET: Projects/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: Projects/Create
-        [HttpPost]
-        public ActionResult Create(FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add insert logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: Projects/Edit/5
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
-
-        // POST: Projects/Edit/5
-        [HttpPost]
-        public ActionResult Edit(int id, FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add update logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: Projects/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
-
-        // POST: Projects/Delete/5
-        [HttpPost]
-        public ActionResult Delete(int id, FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add delete logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
-    }
+		public ActionResult KrohnDesignsOrdersJava()
+		{
+			return View();
+		}
+	}
 }
